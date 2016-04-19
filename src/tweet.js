4113b2992ea79b12568bfed7d92af988ebd9195f
roryg11 <Rory.C.Grant@gmail.com>
2016-04-18 19:51:41 -0700

    finishing react tutorial

--- src/tweet.js
+++ src/tweet.js
@@ -9,15 +9,15 @@ var Twitter = React.createClass({
       }.bind(this)
     );
   },
-  // handleTweetSubmit: function (author, text) {
-  //   var tweet = { author: author, text: text };
-  //
-  //   // POST to add tweet to database
-  //   $.post(this.props.url, tweet, function (data) {
-  //       // Set state in step 10 of the exercise!
-  //     }.bind(this)
-  //   );
-  // },
+  handleTweetSubmit: function (author, text) {
+    var tweet = { author: author, text: text };
+
+    // POST to add tweet to database
+    $.post(this.props.url, tweet, function (data) {
+        this.setState({data: data});
+      }.bind(this)
+    );
+  },
   componentDidMount: function () {
     this.state.data = this.loadTweetsFromServer();
   },
@@ -25,10 +25,9 @@ var Twitter = React.createClass({
     return { data: []}
   },
   render: function () {
-    console.log(this.state.data);
     return (<div className="twitter">
         <h1>Tweets</h1>
-        <TweetForm />
+        <TweetForm onTweetSubmit={this.handleTweetSubmit}/>
         <TweetList data={this.state.data}/>
       </div>
     );
@@ -36,10 +35,23 @@ var Twitter = React.createClass({
 });
 
 var TweetForm = React.createClass({
+  handleSubmit: function(e){
+    e.preventDefault
+    var author = this.refs.author.value;
+    var text = this.refs.text.value;
+    if(!text || !author){
+      return;
+    }
+    this.props.onTweetSubmit(author, text);
+    this.refs.author.value = "";
+    this.refs.text.value = "";
+  },
   render: function () {
     return (
-      <form className="tweetForm">
-        <p>The Tweet Form</p>
+      <form className="tweetForm" onSubmit={this.handleSubmit}>
+        <input text="author" name="author" ref="author"/>
+        <input text="text" name="text" ref="text"/>
+        <button className="btn btn-info">Submit</button>
       </form>
     );
   }
